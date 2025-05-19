1500f464fb3e439ad44fd3111e4a18d4a625a043
juniorrodes <juniorrodes@gmail.com>
2025-05-18 23:51:24 -0300

    Session things

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require('plugins')
 require('colorscheme')
 require('mappings')
+require('commands')
 
 local vim_options_path = string.format("%s/core/options.vim", vim.fn.stdpath("config"))
 local vim_options_cmd = "source " .. vim_options_path
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,1 +1,3 @@
 vim.keymap.set('n', 'gD', function() vim.diagnostic.open_float() end)
+
+vim.keymap.set('n', '<C-W>gn', '<cmd>tab split<cr>')
